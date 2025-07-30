8577d5a1719a12eb0136e92fd6a8ef04ac3e89dc
Sebastian Jeltsch <sebastian@trailbase.io>
2025-07-30 12:39:44 +0200

    Hook up some more or less noop test.

--- runtime-factors/src/build.rs
+++ runtime-factors/src/build.rs
@@ -1,6 +1,6 @@
 use std::path::PathBuf;
 
-use super::{TriggerAppArgs, TriggerFactors, TriggerFactorsRuntimeConfig};
+use super::{TriggerFactors, TriggerFactorsRuntimeConfig};
 
 use anyhow::Context as _;
 use spin_factors::RuntimeFactors;
@@ -10,9 +10,9 @@ use spin_factors_executor::FactorsExecutor;
 // NOTE: crate spin-trigger pulls in spin-factor-sqlite.
 #[cfg(feature = "spin_trigger")]
 use spin_trigger::cli::{
-    FactorsConfig, InitialKvSetterHook, KeyValueDefaultStoreSummaryHook, MaxInstanceMemoryHook,
-    RuntimeFactorsBuilder, SqlStatementExecutorHook, SqliteDefaultStoreSummaryHook,
-    StdioLoggingExecutorHooks,
+  FactorsConfig, InitialKvSetterHook, KeyValueDefaultStoreSummaryHook, MaxInstanceMemoryHook,
+  RuntimeFactorsBuilder, SqlStatementExecutorHook, SqliteDefaultStoreSummaryHook,
+  StdioLoggingExecutorHooks,
 };
 
 /// A [`RuntimeFactorsBuilder`] for [`TriggerFactors`].
@@ -21,73 +21,73 @@ pub struct FactorsBuilder;
 pub struct MyRuntimeConfig;
 
 impl Into<<TriggerFactors as RuntimeFactors>::RuntimeConfig> for MyRuntimeConfig {
-    fn into(self) -> <TriggerFactors as RuntimeFactors>::RuntimeConfig {
-        return TriggerFactorsRuntimeConfig {
-            wasi: None,
-            variables: None,
-            key_value: None,
-            outbound_networking: None,
-            outbound_http: None,
-        };
-    }
+  fn into(self) -> <TriggerFactors as RuntimeFactors>::RuntimeConfig {
+    return TriggerFactorsRuntimeConfig {
+      wasi: None,
+      variables: None,
+      key_value: None,
+      outbound_networking: None,
+      outbound_http: None,
+    };
+  }
 }
 
 #[cfg(feature = "spin_trigger")]
 impl RuntimeFactorsBuilder for FactorsBuilder {
-    type CliArgs = TriggerAppArgs;
-    type Factors = TriggerFactors;
-    // type RuntimeConfig = ResolvedRuntimeConfig<TriggerFactorsRuntimeConfig>;
-    type RuntimeConfig = MyRuntimeConfig;
+  type CliArgs = super::TriggerAppArgs;
+  type Factors = TriggerFactors;
+  // type RuntimeConfig = ResolvedRuntimeConfig<TriggerFactorsRuntimeConfig>;
+  type RuntimeConfig = MyRuntimeConfig;
 
-    fn build(
-        config: &FactorsConfig,
-        args: &Self::CliArgs,
-    ) -> anyhow::Result<(Self::Factors, Self::RuntimeConfig)> {
-        // let runtime_config = Self::RuntimeConfig::from_file(
-        //     config.runtime_config_file.clone().as_deref(),
-        //     config.local_app_dir.clone().map(PathBuf::from),
-        //     config.state_dir.clone(),
-        //     config.log_dir.clone(),
-        // )?;
-        // runtime_config.summarize(config.runtime_config_file.as_deref());
-        let runtime_config = MyRuntimeConfig {};
+  fn build(
+    config: &FactorsConfig,
+    args: &Self::CliArgs,
+  ) -> anyhow::Result<(Self::Factors, Self::RuntimeConfig)> {
+    // let runtime_config = Self::RuntimeConfig::from_file(
+    //     config.runtime_config_file.clone().as_deref(),
+    //     config.local_app_dir.clone().map(PathBuf::from),
+    //     config.state_dir.clone(),
+    //     config.log_dir.clone(),
+    // )?;
+    // runtime_config.summarize(config.runtime_config_file.as_deref());
+    let runtime_config = MyRuntimeConfig {};
 
-        let factors = TriggerFactors::new(
-            None,
-            // runtime_config.state_dir(),
-            config.working_dir.clone(),
-            args.allow_transient_write,
-        )
-        .context("failed to create factors")?;
-        Ok((factors, runtime_config))
-    }
+    let factors = TriggerFactors::new(
+      None,
+      // runtime_config.state_dir(),
+      config.working_dir.clone(),
+      args.allow_transient_write,
+    )
+    .context("failed to create factors")?;
+    Ok((factors, runtime_config))
+  }
 
-    fn configure_app<U: Send + 'static>(
-        executor: &mut FactorsExecutor<Self::Factors, U>,
-        runtime_config: &Self::RuntimeConfig,
-        config: &FactorsConfig,
-        args: &Self::CliArgs,
-    ) -> anyhow::Result<()> {
-        // executor.add_hooks(StdioLoggingExecutorHooks::new(
-        //     config.follow_components.clone(),
-        //     runtime_config.log_dir(),
-        // ));
-        // executor.add_hooks(SqlStatementExecutorHook::new(
-        //     args.sqlite_statements.clone(),
-        // ));
-        // executor.add_hooks(InitialKvSetterHook::new(args.key_values.clone()));
-        // executor.add_hooks(SqliteDefaultStoreSummaryHook);
-        // executor.add_hooks(KeyValueDefaultStoreSummaryHook);
-        //
-        // let max_instance_memory = args
-        //     .max_instance_memory
-        //     .or(runtime_config.max_instance_memory());
-        //
-        // // Only add the hook if a max instance memory size is specified via flag or runtime config.
-        // if let Some(max_instance_memory) = max_instance_memory {
-        //     executor.add_hooks(MaxInstanceMemoryHook::new(max_instance_memory));
-        // }
+  fn configure_app<U: Send + 'static>(
+    executor: &mut FactorsExecutor<Self::Factors, U>,
+    runtime_config: &Self::RuntimeConfig,
+    config: &FactorsConfig,
+    args: &Self::CliArgs,
+  ) -> anyhow::Result<()> {
+    // executor.add_hooks(StdioLoggingExecutorHooks::new(
+    //     config.follow_components.clone(),
+    //     runtime_config.log_dir(),
+    // ));
+    // executor.add_hooks(SqlStatementExecutorHook::new(
+    //     args.sqlite_statements.clone(),
+    // ));
+    // executor.add_hooks(InitialKvSetterHook::new(args.key_values.clone()));
+    // executor.add_hooks(SqliteDefaultStoreSummaryHook);
+    // executor.add_hooks(KeyValueDefaultStoreSummaryHook);
+    //
+    // let max_instance_memory = args
+    //     .max_instance_memory
+    //     .or(runtime_config.max_instance_memory());
+    //
+    // // Only add the hook if a max instance memory size is specified via flag or runtime config.
+    // if let Some(max_instance_memory) = max_instance_memory {
+    //     executor.add_hooks(MaxInstanceMemoryHook::new(max_instance_memory));
+    // }
 
-        Ok(())
-    }
+    Ok(())
+  }
 }
--- runtime-factors/src/lib.rs
+++ runtime-factors/src/lib.rs
@@ -4,7 +4,6 @@ pub use build::FactorsBuilder;
 
 use std::path::PathBuf;
 
-use anyhow::Context as _;
 use spin_common::arg_parser::parse_kv;
 use spin_factor_key_value::KeyValueFactor;
 // use spin_factor_llm::LlmFactor;
@@ -22,92 +21,93 @@ use spin_factors::RuntimeFactors;
 
 #[derive(RuntimeFactors)]
 pub struct TriggerFactors {
-    pub wasi: WasiFactor,
-    pub variables: VariablesFactor,
-    pub key_value: KeyValueFactor,
-    pub outbound_networking: OutboundNetworkingFactor,
-    pub outbound_http: OutboundHttpFactor,
-    // pub sqlite: SqliteFactor,
-    // pub redis: OutboundRedisFactor,
-    // pub mqtt: OutboundMqttFactor,
-    // pub pg: OutboundPgFactor,
-    // pub mysql: OutboundMysqlFactor,
-    // pub llm: LlmFactor,
+  pub wasi: WasiFactor,
+  pub variables: VariablesFactor,
+  pub key_value: KeyValueFactor,
+  pub outbound_networking: OutboundNetworkingFactor,
+  pub outbound_http: OutboundHttpFactor,
+  // pub sqlite: SqliteFactor,
+  // pub redis: OutboundRedisFactor,
+  // pub mqtt: OutboundMqttFactor,
+  // pub pg: OutboundPgFactor,
+  // pub mysql: OutboundMysqlFactor,
+  // pub llm: LlmFactor,
 }
 
 impl TriggerFactors {
-    pub fn new(
-        state_dir: Option<PathBuf>,
-        working_dir: impl Into<PathBuf>,
-        allow_transient_writes: bool,
-    ) -> anyhow::Result<Self> {
-        Ok(Self {
-            wasi: wasi_factor(working_dir, allow_transient_writes),
-            variables: VariablesFactor::default(),
-            key_value: KeyValueFactor::new(),
-            outbound_networking: outbound_networking_factor(),
-            outbound_http: OutboundHttpFactor::default(),
-            // sqlite: SqliteFactor::new(),
-            // redis: OutboundRedisFactor::new(),
-            // mqtt: OutboundMqttFactor::new(NetworkedMqttClient::creator()),
-            // pg: OutboundPgFactor::new(),
-            // mysql: OutboundMysqlFactor::new(),
-            // llm: LlmFactor::new(
-            //     spin_factor_llm::spin::default_engine_creator(state_dir)
-            //         .context("failed to configure LLM factor")?,
-            // ),
-        })
-    }
+  pub fn new(
+    state_dir: Option<PathBuf>,
+    working_dir: impl Into<PathBuf>,
+    allow_transient_writes: bool,
+  ) -> anyhow::Result<Self> {
+    Ok(Self {
+      wasi: wasi_factor(working_dir, allow_transient_writes),
+      variables: VariablesFactor::default(),
+      key_value: KeyValueFactor::new(),
+      outbound_networking: outbound_networking_factor(),
+      outbound_http: OutboundHttpFactor::default(),
+      // sqlite: SqliteFactor::new(),
+      // redis: OutboundRedisFactor::new(),
+      // mqtt: OutboundMqttFactor::new(NetworkedMqttClient::creator()),
+      // pg: OutboundPgFactor::new(),
+      // mysql: OutboundMysqlFactor::new(),
+      // llm: LlmFactor::new(
+      //     spin_factor_llm::spin::default_engine_creator(state_dir)
+      //         .context("failed to configure LLM factor")?,
+      // ),
+    })
+  }
 }
 
 fn wasi_factor(working_dir: impl Into<PathBuf>, allow_transient_writes: bool) -> WasiFactor {
-    WasiFactor::new(SpinFilesMounter::new(working_dir, allow_transient_writes))
+  WasiFactor::new(SpinFilesMounter::new(working_dir, allow_transient_writes))
 }
 
 fn outbound_networking_factor() -> OutboundNetworkingFactor {
-    fn disallowed_host_handler(scheme: &str, authority: &str) {
-        let host_pattern = format!("{scheme}://{authority}");
-        tracing::error!("Outbound network destination not allowed: {host_pattern}");
-        if scheme.starts_with("http") && authority == "self" {
-            terminal::warn!(
-                "A component tried to make an HTTP request to its own app but it does not have permission."
-            );
-        } else {
-            terminal::warn!(
-                "A component tried to make an outbound network connection to disallowed destination '{host_pattern}'."
-            );
-        };
-        eprintln!(
-            "To allow this request, add 'allowed_outbound_hosts = [\"{host_pattern}\"]' to the manifest component section."
-        );
-    }
+  fn disallowed_host_handler(scheme: &str, authority: &str) {
+    let host_pattern = format!("{scheme}://{authority}");
+    tracing::error!("Outbound network destination not allowed: {host_pattern}");
+    if scheme.starts_with("http") && authority == "self" {
+      terminal::warn!(
+        "A component tried to make an HTTP request to its own app but it does not have permission."
+      );
+    } else {
+      terminal::warn!(
+        "A component tried to make an outbound network connection to disallowed destination '{host_pattern}'."
+      );
+    };
+    eprintln!(
+      "To allow this request, add 'allowed_outbound_hosts = [\"{host_pattern}\"]' to the manifest component section."
+    );
+  }
 
-    let mut factor = OutboundNetworkingFactor::new();
-    factor.set_disallowed_host_handler(disallowed_host_handler);
-    factor
+  let mut factor = OutboundNetworkingFactor::new();
+  factor.set_disallowed_host_handler(disallowed_host_handler);
+  factor
 }
 
 /// Options for building a [`TriggerFactors`].
+#[cfg(feature = "spin_trigger")]
 #[derive(Default, clap::Args)]
 pub struct TriggerAppArgs {
-    /// Set the static assets of the components in the temporary directory as writable.
-    #[clap(long = "allow-transient-write")]
-    pub allow_transient_write: bool,
+  /// Set the static assets of the components in the temporary directory as writable.
+  #[clap(long = "allow-transient-write")]
+  pub allow_transient_write: bool,
 
-    /// Set a key/value pair (key=value) in the application's
-    /// default store. Any existing value will be overwritten.
-    /// Can be used multiple times.
-    #[clap(long = "key-value", parse(try_from_str = parse_kv))]
-    pub key_values: Vec<(String, String)>,
+  /// Set a key/value pair (key=value) in the application's
+  /// default store. Any existing value will be overwritten.
+  /// Can be used multiple times.
+  #[clap(long = "key-value", parse(try_from_str = parse_kv))]
+  pub key_values: Vec<(String, String)>,
 
-    /// Run a SQLite statement such as a migration against the default database.
-    /// To run from a file, prefix the filename with @ e.g. spin up --sqlite @migration.sql
-    #[clap(long = "sqlite")]
-    pub sqlite_statements: Vec<String>,
+  /// Run a SQLite statement such as a migration against the default database.
+  /// To run from a file, prefix the filename with @ e.g. spin up --sqlite @migration.sql
+  #[clap(long = "sqlite")]
+  pub sqlite_statements: Vec<String>,
 
-    /// Sets the maxmimum memory allocation limit for an instance in bytes.
-    #[clap(long, env = "SPIN_MAX_INSTANCE_MEMORY")]
-    pub max_instance_memory: Option<usize>,
+  /// Sets the maxmimum memory allocation limit for an instance in bytes.
+  #[clap(long, env = "SPIN_MAX_INSTANCE_MEMORY")]
+  pub max_instance_memory: Option<usize>,
 }
 
 // impl From<ResolvedRuntimeConfig<TriggerFactorsRuntimeConfig>> for TriggerFactorsRuntimeConfig {
@@ -123,3 +123,86 @@ pub struct TriggerAppArgs {
 //         Self::from_source(value)
 //     }
 // }
+
+#[cfg(test)]
+mod tests {
+  use spin_app::{App, AppComponent};
+  use spin_core::{Component, async_trait};
+  use spin_factor_wasi::WasiFactor;
+  use spin_factors_executor::{ComponentLoader, FactorsExecutor};
+  use spin_factors_test::TestEnvironment;
+  use std::sync::Arc;
+
+  use super::*;
+
+  #[tokio::test]
+  async fn instance_builder_works() -> anyhow::Result<()> {
+    let cwd = std::env::current_dir().unwrap();
+
+    let factors = TriggerFactors::new(
+      /* state_dir= */ Some(cwd.clone()),
+      /* working_dir= */ cwd,
+      /* allow_transient_writes */ true,
+    )
+    .unwrap();
+
+    let env = TestEnvironment::new(factors);
+    let locked = env.build_locked_app().await?;
+    let app = App::new(/*id=*/ "test-app", locked);
+
+    let engine_builder = spin_core::Engine::builder(&Default::default())?;
+    let executor = Arc::new(FactorsExecutor::new(engine_builder, env.factors)?);
+
+    struct DummyComponentLoader {}
+
+    #[async_trait]
+    impl ComponentLoader<TriggerFactors, ()> for DummyComponentLoader {
+      async fn load_component(
+        &self,
+        engine: &spin_core::wasmtime::Engine,
+        _component: &AppComponent,
+      ) -> anyhow::Result<Component> {
+        return Component::new(engine, b"(component)");
+      }
+    }
+
+    let factors_app = executor
+      .load_app(
+        app,
+        TriggerFactorsRuntimeConfig::default(),
+        &DummyComponentLoader {},
+      )
+      .await?;
+
+    let (instance, mut store) = {
+      let mut instance_builder = factors_app.prepare(/*component_id=*/ "empty")?;
+
+      assert_eq!(instance_builder.app_component().id(), "empty");
+
+      instance_builder.store_builder().max_memory_size(1_000_000);
+
+      instance_builder
+        .factor_builder::<WasiFactor>()
+        .unwrap()
+        .args(["foo"]);
+
+      instance_builder.instantiate(()).await?
+    };
+
+    assert!(
+      instance
+        .get_export_index(&mut store, None, "wasi:cli/run@0.2.0")
+        .is_none()
+    );
+
+    // let func = instance
+    //   .get_export_index(&mut store, None, "wasi:cli/run@0.2.0");
+    //   .and_then(|i| instance.get_export_index(&mut store, Some(&i), "run"))
+    //   .context("missing the expected 'wasi:cli/run@0.2.0/run' function")?;
+    //
+    // instance.get_typed_func::<(), (Result<(), ()>,)>(&mut store, &func)?
+    // let err = func.call_async(&mut store, ()).await?;
+
+    return Ok(());
+  }
+}
